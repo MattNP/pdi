c31ea44da8525ddd20eadfe819772d5cadbf96b3
juanArias8 <juasda96@gmail.com>
2016-09-15 23:50:14 -0500

    15/09/2016  11:50pm

--- tarea-1/gui.m
+++ tarea-1/gui.m
@@ -66,7 +66,7 @@ guidata(hObject, handles);
 % imagesc(handles.imagen)    %La mostramos en el axis1
 % axis off %Quitamos los ejes
 
-imagen = imread('baile.jpeg');
+%imagen = imread('baile.jpeg');
 % sectors = imread('sectors.png');
 % a = resize(imagen,[640 480]);
 % b = resize(sectors,[640 480]);
@@ -91,6 +91,7 @@ preview(vid,hImage);
 
 %Ocultamos el text7
  set(handles.text7,'visible','off');
+ set(handles.text9,'visible','off');
  
 %Ponemos un borde blanco en axes1
 axes(handles.axes1)
@@ -104,26 +105,11 @@ handles.ax = gca;
 handles.ax.XColor = 'white';
 handles.ax.YColor = 'white';
 
-% for i = 1:100
-%       set(hObject,'BackgroundColor','white');  
-%     set(handles.axes1,'Color',[0 0 0]);
-%     pause(0.1);
-%     set(handles.axes1,'Color',[0 0 1]);
-%     pause(0.1);
-%     set(handles.axes1,'Color',[0 1 0]);
-%     pause(0.1);
-%     set(handles.axes1,'Color',[0 1 1]);
-%     pause(0.1);
-%     set(handles.axes1,'Color',[1 0 0]);
-%     pause(0.1);
-%     set(handles.axes1,'Color',[1 0 1]);
-%     pause(0.1);
-%     set(handles.axes1,'Color',[1 1 0]);
-%     pause(0.1);
-%     set(handles.axes1,'Color',[1 1 1]);
-%     pause(0.1);
-% end
-
+%ponemos un borde blanco en axes2
+axes(handles.axes2)
+handles.ax = gca;
+handles.ax.XColor = 'white';
+handles.ax.YColor = 'white';
 
 % UIWAIT makes gui wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
@@ -147,73 +133,78 @@ function start_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-%Cargamos los botones
-%b = imread('resources/blue.png');
-r = imread('resources/red.png');
-y = imread('resources/yellow.png');
-g = imread('resources/green.png');
-
-
-%Dibujamos un circulo en el Axes 6
-axes(handles.axes6);
-theta = linspace(0, 2*pi);
-x=1+0.5*cos(theta);
-y=2+0.5*sin(theta);
-plot(x,y);
-fill(x,y,'r');      %Función para rellenar la b indica blue
-axis equal
-set(gca,'XTickLabel',[]);
-set(gca,'YTickLabel',[]);
-axis off
-pause(1);
-
-%Pintamos el circulo de rojo
-fill(x,y,'orange');
-axis equal
-set(gca,'XTickLabel',[]);
-set(gca,'YTickLabel',[]);
-axis off
-pause(1);
-
-%Pintamos el circulo de verde
-fill(x,y,'y');
-axis equal
-set(gca,'XTickLabel',[]);
-set(gca,'YTickLabel',[]);
-axis off
-pause(1);
-
-%Pintamos el circulo de Amarillo
-fill(x,y,'g');
-axis equal
-set(gca,'XTickLabel',[]);
-set(gca,'YTickLabel',[]);
-axis off
-
+% %Dibujamos un circulo en el Axes 6
+% axes(handles.axes6);
+% theta = linspace(0, 2*pi);
+% x=1+0.5*cos(theta);
+% y=2+0.5*sin(theta);
+% plot(x,y);
+% 
+% %Pintamos el circulo de rojo
+% fill(x,y,'r');      
+% axis equal
+% set(gca,'XTickLabel',[]);
+% set(gca,'YTickLabel',[]);
+% axis off
+% pause(1);
+% 
+% %Pintamos el circulo de Amarillo
+% fill(x,y,'y');
+% axis equal
+% set(gca,'XTickLabel',[]);
+% set(gca,'YTickLabel',[]);
+% axis off
+% pause(1);
+% 
+% %Pintamos el circulo de Verde
+% fill(x,y,'g');
+% axis equal
+% set(gca,'XTickLabel',[]);
+% set(gca,'YTickLabel',[]);
+% axis off
+% 
 
 set(handles.text8,'String','En juego');
 
 %Creamos un conteo regresivo desde 5
 set(handles.text7,'visible','on');  %hacemos visible text7
+set(handles.text9,'visible','on');
 for i = 5:-1: 0                     
     set(handles.text7,'String',i);  %Enviamos el valor de i a la etiqueta
+    if(i==5)
+        set(handles.text9,'String','PREPARADOS');
+    elseif(i==3)
+        set(handles.text9,'String','LISTOS');
+    elseif(i==0)
+        set(handles.text9,'String','¡¡¡¡YAAAAA!!!!');
+    end
    pause(1)
 end
 set(handles.text7,'visible','off'); %Ocultamos text7
+set(handles.text9,'visible','off');
 
 %Acomodamos la imagen con los puntos de los sensores
 axes(handles.axes1)
-handles.imagen = imread('baile.jpeg');
-handles.imagenag = imread('resources/botones.png');
-a = imresize(handles.imagen,[480 640]);
-b = imresize(handles.imagenag,[480 640]);
-ima = a+b;
-imagesc(ima)
+handles.imagen = imread('resources/baile64.png');
+handles. imagen = imresize(handles.imagen,[640 480]);
+imagesc(handles.imagen)
 axis off
 
 %Cargamos la canción a reproducir 
 cancion = audioread('twice.wav');
 sound(cancion,44100);               %Reproducimos la canción a una frecuencia de 44100Ghz
+
+
+for i = 1:1000 
+    set(handles.axes6,'Color','r');
+    pause(1);
+    set(handles.axes6,'Color','y');
+    pause(1);
+    set(handles.axes6,'Color','b');
+    pause(1);
+    set(handles.axes6,'Color','g');
+    pause(1);
+end
 
 % --- Executes on button press in restart.
 function restart_Callback(hObject, eventdata, handles)
