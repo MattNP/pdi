05a8f146c9b995925d4345837edf934fe5052294
Mateo Noreña <npmatt23@gmail.com>
2016-10-31 01:07:48 -0500

    Revert "Revert "Refactor""
    This reverts commit 29a8a5fcb869d80b11589a3ecf02517b2439321e.

--- tarea-2/normaliza.m
+++ tarea-2/normaliza.m
@@ -1,5 +1,22 @@
+%--------------------------------------------------------------------------
+%------- TRABAJO 2 --------------------------------------------------------
+%------- Clasificación de esquejes ----------------------------------------
+%------- Por: Mateo Noreña Pino    mateo.norena@udea.edu.co ---------------
+%-------      Estudiante Ingeniería de Sistemas  --------------------------
+%-------      CC 1017221148, Wpp 3117597936 -------------------------------
+%------------ John Edisson Tapias Zarrazola    jedisson.tapias@udea.edu.co 
+%-------      Profesor Facultad de Ingenieria BLQ 21-409  -----------------
+%-------      Estudiante Ingeniería de Sistemas  --------------------------
+%-------      CC 1152205006, Wpp xxxxxxxxxx -------------------------------
+%------- Curso BÃ¡sico de Procesamiento de ImÃ¡genes y VisiÃ³n Artificial--
+%------- V1 Octubre de 2016------------------------------------------------
+%--------------------------------------------------------------------------
+
 function d = normaliza(e)
-e = double(e);
-e = e/max(e(:))*255;
-d = uint8(e);
+
+% --- Normaliza una imagen
+
+e = double(e);          % --- Convierte los valores en valores de coma flotante
+e = e/max(e(:))*255;    % --- Normaliza respecto al máximo valor de la imagen
+d = uint8(e);           % --- Retorna la imagen con los valores en uint8
 end
